ca3b9ce730c7ad98b4a212dbb4a69c89befb04ca
luis guilherme <luis-guilherme.pereira@unesp.br>
2021-09-16 22:05:59 -0300

    require include

---
+++ private/shared/staff_footer.php
@@ -0,0 +1,7 @@
+<!doctype html>
+
+<html lang="en">
+	<footer>
+		&copy; <?php echo date('Y'); ?> Globe Bank
+	</footer>
+</html>
--- public/staff/index.php
+++ public/staff/index.php
@@ -1,28 +1,9 @@
-<!doctype html>
+<?php require_once('../../private/initialize.php'); ?>
 
+<?php include('../../private/shared/staff_header.php'); ?>
 <html lang="en">
-  <head>
-    <title>GBI</title>
-    <meta charset="utf-8">
-	<link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
-  </head>
-
-  <body>
-	<header>
-		<h1>GBI Staff Area</h1>
-	</header>
-	<navigation>
-	<ul>
-		<li><a href="index.php">menu</a></li>
-	</ul>
-	</navigation>
-
-	<div id="content">
+		<div id="content">
 		
-	</div>
-
-	<footer>
-		&copy; <?php echo date('Y'); ?> Globe Bank
-	</footer>
-  </body>
+		</div>
 </html>
+<?php include('../../private/shared/staff_footer.php'); ?>
