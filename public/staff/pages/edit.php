11f4ea1f6be7d3de994af95976743b70b021cdb5
luis guilherme <luis-guilherme.pereira@unesp.br>
2021-10-01 23:59:20 -0300

    a

--- public/staff/pages/edit.php
+++ public/staff/pages/edit.php
@@ -5,6 +5,7 @@ require_once('../../../private/initialize.php');
 if(!isset($_GET['id'])) {
 	redirect_to(url_for('/staff/pages/index.php'));
 }
+
 $id = $_GET['id'];
 $menu_name = '';
 $position = '';
